c94221f453ec1ae98dfd08545c0d328eee6fe14d
Ahmed Alani <s3leha@gmail.com>
2021-09-07 15:04:56 +0300

    productItem component impolemented

--- mohim/src/components/ProductItem/index.tsx
+++ mohim/src/components/ProductItem/index.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, Image} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const ProductItem = () => {
+import styles from './styles';
+
+interface ProductItemProps {
+  item: {
+    id: string;
+    title: string;
+    image: string;
+    avgRating: number;
+    ratings?: number | undefined;
+    price: number;
+    oldPrice?: number | undefined;
+  };
+}
+
+const ProductItem = (props: ProductItemProps) => {
+  // props
+  const {item} = props;
+
+  const onPress = () => {
+    //navigate to product details page
+    console.warn('product pressed');
+  };
+
   return (
-    <View>
-      <Text>Product</Text>
-    </View>
+    <Pressable style={styles.root} onPress={onPress}>
+      <Image style={styles.image} source={{uri: item.image}} />
+      <View style={styles.rightContainer}>
+        <Text style={styles.title} numberOfLines={3}>
+          {item.title}
+        </Text>
+        <View style={styles.ratingsContainer}>
+          {/* TODO: Post MVP: half-star logic */}
+          {[1, 2, 3, 4, 5].map((el, i) => (
+            <FontAwesome
+              style={styles.star}
+              name={i < Math.floor(item.avgRating) ? 'star' : 'star-o'}
+              size={18}
+              color={'#fcb249'}
+              key={`${item.id}-${i}`}
+            />
+          ))}
+          <Text>{item.ratings}</Text>
+        </View>
+        <Text style={styles.price}>
+          from ${item.price.toFixed(2)}
+          {item.oldPrice && (
+            <Text style={styles.oldPrice}>${item.oldPrice.toFixed(2)}</Text>
+          )}
+        </Text>
+      </View>
+    </Pressable>
   );
 };
 
--- mohim/src/screens/HomeScreen/index.tsx
+++ mohim/src/screens/HomeScreen/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import styles from './styles';
+import {StyleSheet, SafeAreaView} from 'react-native';
 
 // componenets
 import ProductItem from '../../components/ProductItem';
 
 const HomeScreen = () => {
+  // dummyData
+  const data = {
+    id: '1',
+    title: 'product title',
+    image:
+      'https://mhmt3bucket93316-dev.s3.eu-central-1.amazonaws.com/public/productImages/kent-toot2.jpeg',
+    avgRating: 3,
+    ratings: 5,
+    price: 10.95,
+    oldPrice: 20.95,
+  };
+
   return (
-    <View>
-      <Text style={styles.text}>HomeScreen</Text>
-      <ProductItem />
-    </View>
+    <SafeAreaView style={styles.page}>
+      <ProductItem item={data} />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  page: {
+    paddingHorizontal: 10,
+  },
+  header: {},
+});
 
 export default HomeScreen;
